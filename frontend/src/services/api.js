a560a682dc528c8b3734e2d772e505936c5f017e
bindhumadhuri-1656 <bindhumadhuri85@gmail.com>
2026-05-12 18:37:10 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://socialexpensesplitter-final.onrender.com',
+  baseURL: 'https://socialexpensesplitter-final.onrender.com/api',
 });
 
 // Add a request interceptor to attach JWT token
